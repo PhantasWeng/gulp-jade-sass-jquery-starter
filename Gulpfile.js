1c432f2acb306bc50c829af6fbc7200415606cc5
PhantasWeng <bensonuong@gmail.com>
2016-10-06 15:57:31 +0800

    Change assest path to images

--- Gulpfile.js
+++ Gulpfile.js
@@ -78,8 +78,8 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('copy', function () {
-  gulp.src(['src/assets/**/*.*'])
-    .pipe(gulp.dest('public/assets'));
+  gulp.src(['src/images/**/*.*'])
+    .pipe(gulp.dest('public/images'));
 });
 
 gulp.task('build:dev', ['jade', 'sass', 'js', 'bower', 'copy']);
@@ -89,7 +89,7 @@ gulp.task('serve', ['build:dev'], function () {
   gulp.watch(['src/**/*.jade'], ['jade']).on('change', $.browserSync.reload);
   gulp.watch(['src/**/*.sass'], ['sass']).on('change', $.browserSync.reload);
   gulp.watch(['src/**/*.js'], ['js']).on('change', $.browserSync.reload);
-  gulp.watch(['src/assets/*.*'], ['copy']).on('change', $.browserSync.reload);
+  gulp.watch(['src/images/*.*'], ['copy']).on('change', $.browserSync.reload);
 });
 
 gulp.task('default', ['clean'], function () {
